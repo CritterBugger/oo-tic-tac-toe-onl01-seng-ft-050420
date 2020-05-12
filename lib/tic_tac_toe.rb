50df80c905de65e9f5712542944c3d3c38f000a7
Luke Chen <luke.chen.2010@gmail.com>
2020-05-12 23:06:47 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -18,11 +18,11 @@ class TicTacToe
   end
   
   def display_board
-    print " #{board[0]} | #{board[1]} | #{board[2]} "
+    print " #{@board[0]} | #{@board[1]} | #{@board[2]} "
     print "-----------"
-    print " #{board[3]} | #{board[4]} | #{board[5]} "
+    print " #{@board[3]} | #{@board[4]} | #{@board[5]} "
     print "-----------"
-    print " #{board[6]} | #{board[7]} | #{board[8]} "
+    print " #{@board[6]} | #{@board[7]} | #{@board[8]} "
   end
   
   def input_to_index(input)
